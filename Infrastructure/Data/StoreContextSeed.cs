1448706857bc09315782dabe6cf8755d7f88556f
A2andil <A77mad.22andil@gmail.com>
2020-08-11 11:33:47 +0200

    infrastructure first commit

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandsData 
+                        = File.ReadAllText(@"../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    foreach (var item in brands)
+                    {
+                        ProductBrand brand = new ProductBrand
+                        {
+                            Name = item.Name
+                        };
+                        context.ProductBrands.Add(brand);
+                    }
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch(Exception ex)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Souq/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Souq.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetProducts()
+        {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+    }
+}
