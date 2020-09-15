6c6b011c5dd3e75bfd511c9a00c986f201057788
A2andil <A77mad.22andil@gmail.com>
2020-09-15 23:32:13 +0200

    Infrastructure

---
+++ Core/Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Course : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+
+        }
+
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // You can globally assign schema here
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.HasDefaultSchema("Security");
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
